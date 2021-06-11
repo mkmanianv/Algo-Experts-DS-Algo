c2528839633b883bb5ec6f85362e70208eca6579
mkmanianv <34282318+mkmanianv@users.noreply.github.com>
2021-06-11 21:27:39 +0530

    Create SpiralTraversal.java

---
+++ src/arrays/SpiralTraversal.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Spiral {
+
+    public static List<Integer> spiralTraverse(int[][] array) {
+        List<Integer> result = new ArrayList<>();
+
+        int startRow = 0;
+        int startColumn = 0;
+        int endRow = array.length - 1;
+        int endColumn = array[0].length - 1;
+
+        while (startRow <= endRow && startColumn <= endColumn) {
+            for (int i = startColumn; i <= endColumn; i++) {
+                result.add(array[startRow][i]);
+            }
+            for (int j = startRow + 1; j <= endRow; j++) {
+                result.add(array[j][endColumn]);
+            }
+            for (int i = endColumn - 1; i >= startColumn; i--) {
+                if(startRow==endRow) break;
+                result.add(array[endRow][i]);
+            }
+
+            for (int j = endRow - 1; j > startRow; j--) {
+                if(startColumn==endColumn) break;
+                result.add(array[j][startColumn]);
+            }
+            startRow++;
+            startColumn++;
+            endRow--;
+            endColumn--;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[][] input =
+                new int[][]{
+                        {1, 2, 3, 4},
+                        {10, 11, 12, 5},
+                        {9, 8, 7, 6}
+                };
+        System.out.println(spiralTraverse(input));
+    }
+}
